1691c0c66c35e36aa9f3ae59230df06421ce2ce9
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-09-22 07:25:05 -0300

    feature: whatsapp logo fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Header } from "./sections/Header";
 import { Hero } from "./sections/Hero";
 import { Services } from "./sections/Services";
 
+import WhastAppLogo from './assets/whatsapp.svg'
+
 export function App() {
   return (
     <div className="flex flex-col items-center">
@@ -12,6 +14,9 @@ export function App() {
       <Services />
       <Contact />
       <Footer />
+      <a href="#">
+        <img src={WhastAppLogo} alt="" className="w-16 fixed bottom-10 right-10 max-xs:w-12 max-xs:bottom-2 max-xs:right-2" />
+      </a>
     </div>
   )
 }
