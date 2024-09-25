7de1ca85f6c3eb56f336c1d2ee756a103bdc0f5f
“iPaulinh0” <pauloaraujo2607@gmail.com>
2024-09-25 14:59:14 -0300

    test: window scroll iphone correction

--- src/sections/services/consumer.tsx
+++ src/sections/services/consumer.tsx
@@ -62,7 +62,11 @@ export function Consumer() {
             </AlertDialog>
 
             <Drawer>
-              <DrawerTrigger onClick={() => window.scrollTo(0,0)}
+              <DrawerTrigger onClick={() => window.scroll({
+                top: 0,
+                left: 0,
+                behavior:"smooth"
+              })}
                 className="font-medium text-indigo-blue hidden items-center gap-2 border-b border-blue-dark-sky/0
                   hover:text-blue-dark-sky mt-4 
                   hover:border-blue-dark-sky/100 
